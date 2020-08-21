81df9dce02e48999888fe8683ed1f56c7b8574f6
Luzbelita1975 <lumediaweb@gmail.com>
2020-08-20 22:03:21 -0300

    Add files via upload

---
+++ envia (2).php
@@ -0,0 +1,28 @@
+<?php
+$remitente = $_POST['email'];
+$destinatario = 'lumediaweb@gmail.com'; // en esta línea va el mail del destinatario.
+$asunto = 'Mensaje desde el Sitio Web'; // Personaliza el mensaje que llega al destinatario
+if (!$_POST){
+?>
+
+<?php
+}else{
+	 
+    $cuerpo = "Nombre y apellido: " . $_POST["name"] . "\r\n";
+    $cuerpo .= "Email: " . $_POST["email"] . "\r\n";
+    $cuerpo .= "Celular: " . $_POST["phone"] . "\r\n";
+    $cuerpo .= "Provincia: " . $_POST["provincia"] . "\r\n";
+    $cuerpo .= "Mensaje: " . $_POST["message"] . "\r\n";
+    
+    $headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\n";
+    $headers .= "X-Priority: 3\n";
+    $headers .= "X-MSMail-Priority: Normal\n";
+    $headers .= "X-Mailer: php\n";
+     $headers .= "From: \"".$_POST['name']." <".$remitente.">\n";
+
+    mail($destinatario, $asunto, $cuerpo, $headers);
+    
+    include 'index.html'; // Se puede colocar la pagina personalizada de envio de mensaje.
+}
+?>
